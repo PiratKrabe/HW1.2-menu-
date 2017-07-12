3c498b146cd4115c73f944fa31dc6a0c4db251ed
PiratKrabe <Link_Ex@i.ua>
2017-07-12 15:45:12 +0300

    Update HW.java

--- HW.java
+++ HW.java
@@ -1,53 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
-/*import java.util.Arrays;
-import java.util.Scanner;
-
-/**
- * Created by Linkin on 12.06.2017.
- */
 public class HW {
-    public static void main(String[] args) {
-        int numberMenu;
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Enter size of array :");
-        int x = reader.nextInt();
-        int[] n = new int[x];
-        for (int i = 0; i < n.length; i++) {
-            System.out.println("Enter numbers to array ");
-            n[i] = reader.nextInt();
-        }
-        String arrayNumber = Arrays.toString(n);
-        System.out.println("Your array :" + arrayNumber);
-        System.out.println("Menu ");
-        System.out.println("1.Sort array ");
-        System.out.println("2.Find number to array ");
-        System.out.println("Please enter number of menu : ");
 
-        numberMenu = reader.nextInt();
-        switch (numberMenu) {
-            case 1:
-                String s = Arrays.toString(n);
-                System.out.println("Not sorted array : " + s);
-                Arrays.sort(n);
-                String res = Arrays.toString(n);
-                System.out.println("Sorted arrays :" + res);
+    public static void main(String[] args) {
+        
+        final int arraySize = 5;
+
+        sortBuble(arrayHolder(arraySize));
+
+    }
+    
+    private static int inputIntNumbers() throws Exception {
+        
+        int result;
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        while (true) {
+            try {
+                result = Integer.parseInt(reader.readLine());
                 break;
-            case 2:
-                System.out.println("Enter your number ");
-                int findNumberToArray;
-                int v = 0;
-                findNumberToArray = reader.nextInt();
-                for (int i = 0; i < n.length; i++) {
-                    if (findNumberToArray == n[i]) {
-                        System.out.println("Your element" + findNumberToArray + " in array on position # " + (i+1));
-                        v = v + 1;
-                    }
-                }
-                if (v == 0) {
-                    System.out.println("Element not found in array ");
-                }
+            } catch (NumberFormatException ex) {
+                System.err.println("Invalid input. Enter the number");
+            }
         }
+        return result;
+    }
+
+
+    private static int[] arrayHolder(int arraySize) {
+
+        int arrayInt[] = new int[arraySize];
+
+        int indexOfArray = 0;
+
+        while (indexOfArray < arrayInt.length) {
+            System.out.println("Array size: " + arraySize);
+            System.out.println("Enter " + (indexOfArray + 1) + " element of the array");
+            try {
+                arrayInt[indexOfArray] = inputIntNumbers();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+            indexOfArray++;
+        }
+        
+        System.out.println("Initial array " + Arrays.toString(arrayInt));
+        return arrayInt;
+    }
+
+    private static void sortBuble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            
+            for (int j = arr.length - 2; j >= i; j--) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+        System.out.println("Sorted array " + Arrays.toString(arr));
     }
 }
